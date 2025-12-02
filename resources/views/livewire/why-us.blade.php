885d59138c89f7086a2dadf44f74115971364a58
Susmita277 <susmetaadangal@gmail.com>
2025-12-02 14:55:01 +0545

    add why-us page

---
+++ resources/views/livewire/why-us.blade.php
@@ -0,0 +1,6 @@
+<div>
+    {{-- The best athlete wants his opponent at his best. --}}
+    {{-- I'm rollin' up my broccoli, LFG 🥦 --}}
+    <x-seo :seo="$seo" />
+    <x-page-blocks :blocks="$blocks" />
+</div>
