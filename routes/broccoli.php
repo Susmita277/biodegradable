8d8d8d421cfc64d81e6755fefd9ce71b6306f2ef
Susmita277 <susmetaadangal@gmail.com>
2026-06-30 22:26:01 +0545

    add route for product and auth page

--- routes/broccoli.php
+++ routes/broccoli.php
@@ -10,3 +10,5 @@ Route::get('/', \App\Livewire\Home::class)->name('home');
 Route::get('/contact', \App\Livewire\Contact::class)->name('contact');
 Route::get('/about', \App\Livewire\About::class)->name('about');
 Route::get('/why-us', \App\Livewire\WhyUs::class)->name('why-us');
+Route::get('/product', \App\Livewire\Product::class);
+Route::get('/auth', \App\Livewire\Auth::class);
