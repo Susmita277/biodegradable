383958b7a462741141079dae0bf40f8dd8a0544d
Susmita277 <susmetaadangal@gmail.com>
2026-06-30 22:24:57 +0545

    add new route

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(RunningBroccoli::class)->group(function () {
     require __DIR__.'/broccoli.php';
 });
+Route::get('/products', \App\Livewire\Product::class)->name('products');
+Route::get('/login', \App\Livewire\Auth::class)->name('login');
+Route::get('/register', \App\Livewire\Auth::class)->name('register');
